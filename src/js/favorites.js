a38a0e97153f30ffeeb99e7bb2e87a4c83171c16
SamuelBLuna <samuhluna@gmail.com>
2023-07-05 15:18:24 -0300

    finalizaação do projeto

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,29 +1,67 @@
+import { GithubUser } from "./GIthubUser.js"
+
 export class Favotites {
     constructor(root) {
         this.root = document.querySelector(root)
         this.tbody = this.root.querySelector ('table tbody')
         this.load()
+        this.onadd()
     }
-
+    
     load() {
-        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+        this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || []
     }
 
+    save() {
+        localStorage.setItem("@github-favorites:", JSON.stringify(this.entries))
+    }
+
+    async add(username) {
+        try {
+
+            const userExists = this.entries.find(entry => entry.login == username)
+
+            if(userExists) {
+                throw new Error('Usuário já cadastrado')
+            }
+
+            const user = await GithubUser.search(username)
+            if(user.login === undefined) {
+                throw new Error('Usuário não encontrado')
+            }
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+        }catch(error) {
+            alert(error.message)
+        }
+    }
+    
     delete(user) {
         const filteredEtries = this.entries.filter(entry => entry.login !== user.login)
-
+        
         this.entries = filteredEtries
         this.update()
+        this.save()
     }
 }
 
 export  class FavotitesView extends Favotites {
     constructor(root) {
         super(root)
-
+        
         this.update()
     }
 
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            let textInput = this.root.querySelector('#search').value
+            this.add(textInput)
+            textInput = this.root.querySelector('#search').value =''
+        }
+    }
     update() {
         this.removeAllTr()
 
